dd0aa7b93dd514c824c32666dae4d602b6adec50
Паша <@DESKTOP-3C2NDOK>
2018-09-28 17:12:19 +0300

    Close #6.

--- Film.Data.Xml.XDocument/ReturnIdOfElements.cs
+++ Film.Data.Xml.XDocument/ReturnIdOfElements.cs
@@ -14,34 +14,37 @@ namespace Films.Data.Xml.xDocument
 
         public int GetActorId(string name, string surname)
         {
+            int id = 0;
             IEnumerable<XElement> actors = document.Root.Element("Actors").Elements("Actor");
 
             foreach (XElement actor in actors)
             {
                 if (name.Equals(actor.Attribute("Name").Value) && surname.Equals(actor.Attribute("Surname").Value))
                 {
-                    return int.Parse(actor.Attribute("id").Value);
+                    id = int.Parse(actor.Attribute("id").Value);
                 }
             }
-            return 0;
+            return id;
         }
 
         public int GetLanguageId(string name)
         {
+            int id = 0;
             IEnumerable<XElement> languages = document.Root.Element("Languages").Elements("Language");
 
             foreach (XElement language in languages)
             {
                 if (name.Equals(language.Attribute("Name").Value))
                 {
-                    return int.Parse(language.Attribute("id").Value);
+                    id = int.Parse(language.Attribute("id").Value);
                 }
             }
-            return 0;
+            return id;
         }
 
         public int GetProducerId(string name, string surname)
         {
+            int id = 0;
             IEnumerable<XElement> producers = document.Root.Element("Producers").Elements("Producer");
 
             foreach (XElement producer in producers)
@@ -51,7 +54,7 @@ namespace Films.Data.Xml.xDocument
                     return int.Parse(producer.Attribute("id").Value);
                 }
             }
-            return 0;
+            return id;
         }
     }
 }
